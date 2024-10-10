aee057cf6243ff4192c194872b49e9c1ac40e732
Pity-0 <pityrl20@gmail.com>
2024-10-10 21:18:16 +0100

    Testimonio

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, CardMedia, CardContent, Typography, IconButton, Stack, Box, Paper } from '@mui/material';
+import { Card, CardMedia, CardContent, Typography, IconButton, Stack, Box} from '@mui/material';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
